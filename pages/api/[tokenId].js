ba626f89846e0ee384bb16027e0182cdbf41b59b
HughesSeanKyle <khughessean@yahoo.com>
2022-10-12 16:30:02 +0200

    Metadata api

---
+++ pages/api/[tokenId].js
@@ -0,0 +1,21 @@
+/*
+    Get token id and associated address from here 
+    https://goerli.etherscan.io/token/0xD5357C7235192a870A2720C77EA6de38905b6eF0
+*/
+
+export default function handler(req, res) {
+	// get the tokenId from the query params
+	const tokenId = req.query.tokenId;
+	// As all the images are uploaded on github, we can extract the images from github directly.
+	const image_url =
+		'https://raw.githubusercontent.com/LearnWeb3DAO/NFT-Collection/main/my-app/public/cryptodevs/';
+	// The api is sending back metadata for a Crypto Dev
+	// To make our collection compatible with Opensea, we need to follow some Metadata standards
+	// when sending back the response from the api
+	// More info can be found here: https://docs.opensea.io/docs/metadata-standards
+	res.status(200).json({
+		name: 'Crypto Dev #' + tokenId,
+		description: 'Crypto Dev is a collection of developers in crypto',
+		image: image_url + tokenId + '.svg',
+	});
+}
